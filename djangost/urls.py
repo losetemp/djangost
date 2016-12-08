78fee63aff0039377b632e79f2dedec17599a2b3
losetemp <jjyypi@gmail.com>
2016-12-08 11:37:09 +0800

    add index page

--- djangost/urls.py
+++ djangost/urls.py
@@ -1,7 +1,7 @@
 """djangost URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from deals import views as deals_views
 
 urlpatterns = [
+	url(r'^$', deals_views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
